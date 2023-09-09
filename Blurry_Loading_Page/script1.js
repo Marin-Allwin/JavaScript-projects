a3b198c1580bc5929af678006326aa0b7b49176d
Marin Allwin <allinallwina3935@gmail.com>
2023-09-09 16:04:48 +0530

    First project

---
+++ Blurry_Loading_Page/script1.js
@@ -0,0 +1,22 @@
+let loadText=document.querySelector(".loadingText")
+let bg=document.querySelector(".bg")
+
+let int= setInterval(blurring,20)
+let load=0
+
+function blurring(){
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+
+    loadText.innerText=`${load}%`
+    loadText.style.opacity= scale(load, 0, 100, 1, 0)
+    bg.style.filter=`blur(${scale(load, 0 ,100, 30, 0)}px)`
+
+}
+
+const scale =(num, in_min, in_max, out_min, out_max) => {
+    return(num-in_min) * (out_max -out_min) / (in_max-in_min) + out_min;
+}
